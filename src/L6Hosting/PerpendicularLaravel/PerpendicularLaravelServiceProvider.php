82e024a0589674d6c76dc4f1b1f4fe7b49b2cf15
Travis Beck <tjbenator@gmail.com>
2014-11-15 15:40:28 -0800

    Laravel Base Package

---
+++ src/L6Hosting/PerpendicularLaravel/PerpendicularLaravelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace L6Hosting\PerpendicularLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class PerpendicularLaravelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('l6-hosting/perpendicular-laravel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
