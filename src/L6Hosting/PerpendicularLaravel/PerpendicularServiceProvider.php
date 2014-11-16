c3b24537fcbfd65ba074bb2d941a5b1246ddfc9b
Travis Beck <tjbenator@gmail.com>
2014-11-15 17:22:38 -0800

    Renamed Service Provider

--- src/L6Hosting/PerpendicularLaravel/PerpendicularServiceProvider.php
+++ src/L6Hosting/PerpendicularLaravel/PerpendicularServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class PerpendicularLaravelServiceProvider extends ServiceProvider {
+class PerpendicularProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
